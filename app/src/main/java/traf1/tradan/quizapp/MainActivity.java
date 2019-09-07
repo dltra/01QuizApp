cb663ad7c351a0e35358656d09220e7fd1eaaab4
Dan Tra <dltra@fcps.edu>
2019-09-07 10:56:05 -0400

    Add Behavior - Set OnClickListener and Log

--- app/src/main/java/traf1/tradan/quizapp/MainActivity.java
+++ app/src/main/java/traf1/tradan/quizapp/MainActivity.java
@@ -3,6 +3,8 @@ package traf1.tradan.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton=findViewById(R.id.clickButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton","Hi dad!");
+            }
+        });
     }
 }
