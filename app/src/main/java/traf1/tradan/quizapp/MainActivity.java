e107042863de046d07e3ddedf2e1579e2f67e43b
Dan Tra <dltra@fcps.edu>
2019-09-07 10:35:14 -0400

    Add Behavior - Retrieve Element

--- app/src/main/java/traf1/tradan/quizapp/MainActivity.java
+++ app/src/main/java/traf1/tradan/quizapp/MainActivity.java
@@ -3,12 +3,14 @@ package traf1.tradan.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
     }
 }
